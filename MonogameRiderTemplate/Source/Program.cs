69dfe1d4364f79f74a46d7b926fef86b43eda5dc
Limeoats <admin@limeoats.com>
2018-07-12 10:05:34 -0400

    first commit

---
+++ MonogameRiderTemplate/Source/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace TheVillageOfLife
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Monogame-Rider-Template
